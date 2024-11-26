78c3daa0ec18572171c39e1b37dd347ddc2ea3c7
Bhea Del Rosario <bdelrosario@rrc.ca>
2024-11-26 17:48:37 -0600

    Updated JS and added jest tests

--- WORKS_CONSULTED.md
+++ WORKS_CONSULTED.md
@@ -4,3 +4,7 @@ https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_intro
 ## Bhea
 number input
 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/number
+parseFloat
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
+jest test toEqual
+https://stackoverflow.com/questions/56839801/jest-js-error-received-serializes-to-the-same-string
--- donation-tracker.js
+++ donation-tracker.js
@@ -1,67 +1,128 @@
-const formNode = document.querySelector("#donation-tracker");
 
-const charityNameNode = document.querySelector("#charity-name-section")
-const charityNameInput = document.querySelector("#charity-name");
-
-const donationAmountNode = document.querySelector("#donation-amt-section");
-const donationAmountInput = document.querySelector("#donation-amt");
-
-const donationDateNode = document.querySelector("#donation-date-section");
-const donationDateInput = document.querySelector("#donation-date");
-
-const donorCommentNode = document.querySelector("#donor-comment-section");
-const donorCommentInput = document.querySelector("#donor-comment");
 
 const data = {};
 
-// EVENT LISTENER: FORM SUBMIT
-formNode.addEventListener("submit", (event) =>{
-    event.preventDefault();
+if(typeof window === "undefined") {
+    // window object represents the browser window
+    module.exports = { attachEventListener, 
+        createDataObject, 
+        validateCharityName,
+        validateDonation,
+        validateDate};
+} else {
+    // if undefined, window object is not available
+    // typically result of working in a Node environment
+    window.onload = init;
+}
+
+function init(){
+    const formNode = document.querySelector("#donation-tracker");
+    attachEventListener(formNode, validateFormSubmit);
+}
+
+function attachEventListener(node, callback){
+    
+    node.addEventListener("submit", event => {
+        event.preventDefault();
+        callback();
+    })
+}
+
+function validateFormSubmit(){
+    // reset validity and clear errors
+
     clearErrors();
-    validateCharityName();
-    validateDonation();
-    validateDate();
-    validateComments();
+
+    // Validate inputs
+    const charityNameNode = document.querySelector("#charity-name-section");
+    const charityNameInput = document.querySelector("#charity-name").value;
+    const isCharityNameValid = validateCharityName(charityNameInput, charityNameNode);
+
+    const donationAmountNode = document.querySelector("#donation-amt-section");
+    const donationAmountInput = document.querySelector("#donation-amt").value;
+    const isDonationValid = validateDonation(donationAmountInput, donationAmountNode);
+
+    const donationDateNode = document.querySelector("#donation-date-section");
+    const donationDateInput = document.querySelector("#donation-date").value;
+    const isDateValid = validateDate(donationDateInput, donationDateNode);
+
+    // const donorCommentNode = document.querySelector("#donor-comment-section");
+    const donorCommentInput = document.querySelector("#donor-comment").value;
+    // const isCommentsValid = validateComments(donorCommentInput, donorCommentNode);
+
+    // Check if all validations passsed, doesn't check for comments
+    const isValid = isCharityNameValid && isDonationValid && isDateValid;
+
+    // If all validations passed
+    if(isValid){
+        createDataObject(charityNameInput, donationAmountInput, donationDateInput, donorCommentInput);
+    }
+
+}
+
+function createDataObject(charityName, donationAmt, donationDate, donorComment){
+    const data = {
+        name: charityName,
+        donation: donationAmt,
+        date: donationDate,
+        comment: donorComment
+    }
+    
     console.log(data);
-})
+    return data;
+}
 
-function validateCharityName(){
-    const charityName = escapeHTML(charityNameInput.value);
-
-    if(charityName === ""){
-        showError(charityNameNode, "Charity Name cannot be blank.");
+function validateCharityName(charityName, node){
+    // Check charityName if blank
+    if(charityName.length <= 0){
+        showError(node, "Charity Name cannot be blank.");
+        return false;
     } else {
-        data.name = charityName;
+        return true;
+    }
+
+}
+
+function validateDonation(donationAmount, node){
+    // Floating point numbers
+    const numericPattern = /^-?\d+(\.\d+)?$/;
+    // check if donation is blank, less than $1.00, or non numeric.
+    // Break out of function if any errors
+    if(donationAmount.length <= 0){
+        showError(node, "Donation cannot be blank.");
+        return false;
+    }
+    if(parseFloat(donationAmount) < 1.00){
+        showError(node, "Donation must be at least $1.00");
+        return false;
+    }
+    if(!numericPattern.test(donationAmount)){
+        showError(node, "Donation amount must be numeric.");
+        return false;
+    }
+    
+    return true;
+    
+}
+
+function validateDate(donationDate, node){
+    if(donationDate.length <= 0){
+        showError(node, "A date must be selected.");
+        return false;
+    } else{
+        return true;
     }
 }
 
-function validateDonation(){
-    const donationAmount = escapeHTML(donationAmountInput.value);
-
-    if(donationAmount === ""){
-        showError(donationAmountNode, "Donation cannot be blank.");
-    } else {
-        data.donation = donationAmount;
-    }
-}
-
-function validateDate(){
-    const donationDate = donationDateInput.value;
-    if(donationDate === ""){
-        showError(donationDateNode, "A date must be selected.");
-    } else {
-        data.date = donationDate;
-    }
-}
-
-function validateComments(){
-    const donorComment = escapeHTML(donorCommentInput.value);
-    if(donorComment === ""){
-        showError(donorCommentNode, "Please enter a comment.");
-    } else {
-        data.comment = donorComment;
-    }
-}
+// function validateComments(donorComment, node){
+    
+//     if(donorComment.length <= 0){
+//         showError(node, "Please enter a comment.");
+//         return false;
+//     } else{
+//         return true;
+//     }
+// }
 
 function showError(inputElement, message){
     // Create new element
